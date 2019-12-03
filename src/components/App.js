e03c72304a3622c99150c17ddc84536497f7bee1
Michel Reyes <mreyesh85@gmail.com>
2019-12-03 07:21:38 -0500

    added conect in app & get initialdata

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { handleInitialData } from "../actions/shared";
+import { connect } from "react-redux";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
+    return <div>Starter Code</div>;
   }
 }
 
-export default App
+export default connect()(App);
